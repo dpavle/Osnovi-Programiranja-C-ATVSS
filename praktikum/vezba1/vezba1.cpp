0ccb256469de6f589c757d72d32a9f4ce8c7cbb0
Pavle Denčić <kot6020@vtsnis.rs>
2021-03-04 00:25:58 +0100

    vežba 2, mala rekonstrukcija komentara na prvoj vežbi

--- praktikum/vezba1/vezba1.cpp
+++ praktikum/vezba1/vezba1.cpp
@@ -2,17 +2,24 @@
 
 void razlika_brojeva() {
 	int razlika = 100 - 90;
-	printf("\nRazlika 100 - 90 = %d\n\n", razlika); // Naredba 'printf' je definisana u 'stdio.h' biblioteci. Njena funkcija je štampanje podataka (rezultata) na standardnom izlazu.
+	printf("\nRazlika 100 - 90 = %d\n\n", razlika); 
+
+	/* Naredba 'printf' je definisana u 'stdio.h' biblioteci. 
+	Njena funkcija je štampanje podataka (rezultata) na standardnom izlazu. */ 
 }
 
 void kolicnik_brojeva() {
 	int a = 3, b = 2, c = a / b;
-	printf("\nkolicnik (c) = %d\n", c); // Rezultat deljenja (3 / 2) je 1 zato što su promenljive a,b i c celobrojnog tipa (integer), tako da se ostatak pri deljenju ne prikazuje. 
+	printf("\nkolicnik (c) = %d\n", c); 
+	
+	/* Rezultat deljenja (3 / 2) je 1 zato što su promenljive a, b i c celobrojnog tipa (integer), 
+	tako da se ostatak pri deljenju ne prikazuje. */
 }
 
 void kolicnik_brojeva_2() { 
 	int a = 3, b = 0;
-	// a = a / b; // Deljenje nulom (b = 0) nije moguće, greška.
+	// a = a / b;  
+	/* Deljenje nulom (b = 0) nije moguće, greška. */
 	printf("%d\n", a);
 }
 
