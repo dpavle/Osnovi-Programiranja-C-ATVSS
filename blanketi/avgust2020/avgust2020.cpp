a41a6e8be26eee2885a94415ab2f9889440d966d
Pavle <kot6020@vtsnis.rs>
2021-06-12 00:04:32 +0200

    blanketi avgust2020 i jul2019

---
+++ blanketi/avgust2020/avgust2020.cpp
@@ -0,0 +1,163 @@
+﻿
+#include <iostream>
+
+void z1() {
+	/* u program unosimo tekst do pojavljivanja znaka '.', pri cemu tekst
+	unosimo slovo po slovo. prikazuje broj pojavljivanja svih samoglasnika i 
+	izrazava tu vrednost procentualno u odnosu na sve unete znake. */
+
+	char in; 
+	int a_ct, e_ct, i_ct, o_ct, u_ct, ostalo, ukupno; 
+	a_ct = e_ct = i_ct = o_ct = u_ct = ostalo = ukupno = 0;
+
+	printf("Unesi tekst: ");
+	while ((in = getchar()) != '.') { // tekst se unosi dok se ne unese znak '.'
+		switch (in) { // AEIOU/aeiou - samoglasnici 
+			case 'a': a_ct++; break;
+			case 'A': a_ct++; break;
+			case 'e': e_ct++; break;
+			case 'E': e_ct++; break;
+			case 'i': i_ct++; break;
+			case 'I': i_ct++; break;
+			case 'o': o_ct++; break;
+			case 'O': o_ct++; break;
+			case 'u': u_ct++; break;
+			case 'U': u_ct++; break;
+			default: ostalo++; break; // ako nema samoglasnika, inkrementira se vrednost ostalo, tj. broj suglasnika 
+			}  // kraj switch 
+		ukupno++;
+	}
+
+	printf("A: %d E: %d I: %d O: %d U: %d", a_ct, e_ct, i_ct, o_ct, u_ct);
+	printf("\n%2d%% unesenih karaktera su samoglasnici.\n", ((a_ct + e_ct + i_ct + o_ct + u_ct) * 100) / ukupno); 
+}
+
+bool palindrom(int num) { // zadatak 2.1
+	/* funkcija koja za uneti proizvoljan broj proverava da li je on palindrom. */
+
+	int c, obrnut = 0; 
+	int original = num;
+
+	if (num < 0) { // brojevi manji od 0 ne mogu biti palindomi
+		return false; 
+	}
+	
+	while (num > 0) { 
+		c = num % 10; // izvlacenje cifara 
+		obrnut = obrnut * 10 + c; // 'okretanje' broja
+		num /= 10; 
+	}
+
+	return (original == obrnut); // broj je palindrom ako se moze procitati na isti nacin sa leva na desno i sa desna na levo
+}
+
+void z3() {
+	/* program koji racuna broj elemenata vecih od 10 i deljivih sa 3. 
+	svi takvi elementi se smestaju u novi niz i racuna se njihov zbir.*/
+
+	int b = 0, rez[3 * 4], z = 0; // rez - skup svih elemenata vecih od 10 i deljivih sa 3, z - zbir, b - brojac
+
+	const int matrica[3][4] = 
+	{
+		{1, 2, 3, 4},
+		{5, 6, 7, 8},
+		{9, 10, 11, 12}
+	};
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 4; j++) {
+			if (matrica[i][j] > 10 && matrica[i][j] % 3 == 0) { // uslov zadatka
+				z += matrica[i][j]; // zbir svih clanova koji ispunjavanju uslov
+				rez[b] = matrica[i][j]; // skup svih elemenata koji ispunjavaju uslov
+				b++; // brojac elemenata
+			}
+		}
+	}
+
+	printf("\nElemenata matrice koji su >10 i deljivi sa 3 ima %d, oni su: ", b);
+
+	for (int i = 0; i < b; i++) {
+		printf("%d ", rez[i]);
+	}
+
+	printf("\nNjihov zbir je: %d\n", z);
+}
+
+
+void z4() {
+	/* u ulaznoj datoteci ulaz.txt se nalaze imena gradova u srbiji.
+	u izlaznu datoteku izlaz.txt se prepisuju imena svih gradova koja pocinju na B 
+	ili ona cija je duzina veca od 11 slova.*/
+
+	FILE* ulaz, *izlaz; 
+	char buffer[255];
+
+	ulaz = fopen("ulaz.txt", "r"); izlaz = fopen("izlaz.txt", "w"); // otvaranje fajlova
+
+	while (fgets(buffer, 255, ulaz) != NULL) { // citanje stringova ulaznog fajla red-po-red
+		if ((strlen(buffer) > 11) || buffer[0] == 'B') { // strlen() funkcija vraca duzinu stringa
+			// printf("%s", &buffer[0]); 
+			fputs(buffer, izlaz); // upis stringova koji ispunjavaju uslov iz bafera u izlazni fajl
+		}
+	}
+	fclose(ulaz); fclose(izlaz); // zatvaranje fajlova 
+}
+
+int povrsina_obim_pravougaonika(int a, int b, int c) {
+	/* funkcija koja racuna povrsinu i obim pravougaonika 
+	integer c sluzi za odabir return vrednosti f-je (obim/povrsina) */
+	
+	int obim, povrsina;
+
+	obim = 2 * (a + b); 
+	povrsina = a * b;
+
+	if (c == 1) {
+		return (obim);
+	}
+	else if (c == 2) {
+		return (povrsina);
+	}
+	else {
+		return 0;
+	}
+}
+
+
+int main() {
+	int num[50], n, p = 0, a, b, obim, povrsina, zp = 0;
+
+
+	 z1(); // zadatak 1
+
+	printf("Unesi N: "); scanf_s("%d", &n); // zadatak 2.2
+
+	for (int i = 0; i < n; i++) { // unos brojeva i ispit palindroma
+		scanf_s("%d", &num[i]);
+		if (palindrom(num[i])) {
+			p++; 
+		}
+	}
+	printf("Ima %d palindroma! \n", p);
+	
+	z3(); // zadatak 3
+
+	z4(); // zadatak 4
+
+
+	printf("Unesi N: "); scanf_s("%d", &n); // zadatak 5 
+
+	for (int i = 0; i < n; i++) {
+		printf("Unesi a i b pravouganika: "); scanf_s("%d %d", &a, &b);
+
+		obim = povrsina_obim_pravougaonika(a, b, 1); povrsina = povrsina_obim_pravougaonika(a, b, 2);
+
+		printf("Obim %d. pravougaonika: %d\n", i+1, obim); 
+		
+		zp += povrsina;
+	}
+
+	printf("Zbir povrsina svih pravougaonika je: %d", zp);
+}
+
+
---
+++ blanketi/jul2019/jul2019.cpp
@@ -0,0 +1,228 @@
+﻿#include <iostream>
+#include <math.h> 
+
+#define PI 3.14159265358979323846
+
+// I kolokvijum
+
+long float faktorijel(int x) {
+	/* funkcija za izracunavanje faktorijela broja (X!) */
+
+	int f = 1, i; 
+
+	for (i = 1; i <= x; i++) {
+		f = f * i;
+	}
+	return f; 
+}
+
+float povrsina_lopte(int r) {
+	/* funkcija za izracunavanje povrsine lopte (P = 4 * pi * r^2) */
+	float A; 
+
+	A = 4 * PI * pow(r, 2);
+
+	return A;
+}
+
+
+float zapremina_lopte(int r) {
+	/* funkcija za izracunavanje zapremine lopte (V = 4/3 * pi * r^3) */
+	float V; 
+
+	V = (4 / 3.0) * PI * pow(r,3);
+
+	return V;
+}
+
+int z1() {
+	/* program za izracunavanje sume 
+	S = SUM k -> N : X^K+1 / K!+N-N^2 */
+
+	int N, s = 0, x;
+
+	printf("N: "); scanf("%d", &N);
+	printf("X: "); scanf("%d", &x);
+
+	for (int K=10; K <= N; K++) {
+		s += (pow(x, K+1)) / (faktorijel(K) + N - pow(N, 2));
+	}
+	return s;
+}
+
+void z2(int a, int b) {
+	/* program koji izracunava vrednost f-je 
+	
+		{ e^2a+sin(b), a<b
+	y = { cos^3(a*b), a=b
+		{ sqrt(a-b^3), a>b
+
+	*/
+
+}
+
+void z3() {
+	/* program koji unetu cifru ispisuje morzeovom azbukom.*/
+
+	char ch = getchar();
+	switch (ch) {
+		case '0': printf("-----"); break;
+		case '1': printf(".----"); break;
+		case '2': printf("..---"); break; 
+		case '3': printf("...--"); break;
+		case '4': printf("....-"); break; 
+		case '5': printf("....."); break; 
+		case '6': printf("-...."); break;
+		case '7': printf("--..."); break;
+		case '8': printf("---.."); break;
+		case '9': printf("----."); break;
+	}
+}
+
+void z4() {
+
+}
+
+int z5() {
+	/* program koji racuna zbir n proizvoljno unetih celih brojeva 
+	koriscenjem while petlje */
+	
+	int x, n, z = 0, br = 1;
+
+	printf("Broj brojeva za unos: "); scanf("%d", &n);
+	
+	while (br<=n) {
+		scanf("%d", &x);
+		z += x;
+		br++;
+	}
+	return z; 
+}
+
+// II kolokvijum 
+
+void z6() {
+	/* data je kvadratna matrica, kao izlaz iz programa 
+	je predvidjeno stampanje ove matrice u pokaznom formatu kao i 
+	stampanje nove matrice u istom obliku ali sada unazad od clana a44, a43, a42, a41, ... a11 */
+	const int matrica[4][4]{ 
+		{ 1, 2, 3, 4},
+		{ 5, 6, 7, 8}, 
+		{9, 10, 11, 12}, 
+		{13, 14, 15, 16} 
+	};
+
+	printf("\nUlazni niz: \n");
+
+	for (int i = 0; i < 4; i++) { // stampanje ulaznog niza 
+		for (int j = 0; j < 4; j++) {
+			printf("%d ", matrica[i][j]);
+		}
+		printf("\n");
+	}
+	
+	printf("\nObrnuti izlazni niz: \n");
+
+	for (int i = 3; i >= 0; i--) { // stampanje ulaznog niza u obrnutom obliku
+		for (int j = 3; j >= 0; j--) {
+			printf("%d ", matrica[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void z7() {
+/* program za izracunavanje sume S = SUM k -> N : X^(2*k) * cos(k*2) / k! + N! * 2 */ 
+} 
+
+void z8(const char *ulaz, const char *izlaz) {
+
+	FILE* fp, *fpr;
+	int br;
+
+	fp = fopen(ulaz, "r"); fpr = fopen(izlaz, "w"); 
+
+	while (!feof(fp)) { // program radi dok ne stigne do kraja ulaznog fajla
+		fscanf(fp, "%d", &br);
+		if (br % 3 == 0 && br % 7 == 0) { // ako je uneti broj deljiv sa 3 i 7, upisuje se u izlazni fajl
+			printf("\n%d ", br);
+			fprintf(fpr, "    %d\n", br); // upis u izlazni fajl
+		}	
+	}
+}
+
+void z9() {
+	/* unosi se niz karaktera u vektor Niz 1.
+	sadrzaj vektora Niz1 se kopira u vektor Niz2 i utvrdjuje da li je to isti niz. 
+	broji koliko praznina (blanko karaktera) ima u nizu Niz1 */
+
+	char Niz1[255], Niz2[255]; 
+	int c = 0, b = 0; 
+
+	char ch = getchar(); // unos prvog karaktera
+
+	while (ch != '!') { // program radi dok se ne unese karakter '!'
+		Niz1[c] = ch; c++; // rezultantni niz
+		ch = getchar(); // unos sledeceg karaktera 
+	}
+	Niz1[c++] = '!'; // prepisivanje karaktera ! kojim je zavrsio unos  
+
+	printf("Niz1: ");
+	for (int i = 0; i < c; i++) { // stampanje prvog niza
+		printf("%c", Niz1[i]);
+
+		Niz2[i] = Niz1[i]; // kopiranje vrednosti Niza1 u Niz2 na istoj poziciji
+
+		if (Niz1[i] == ' ') { // brojanje blanko karaktera
+			b++;
+		}
+	}
+
+	printf(" Niz2: ");
+	for (int i = 0; i < c; i++) { // stampanje drugog niza
+		printf("%c", Niz2[i]);
+	}
+
+
+	printf("\nDuzina niza 1 je: %d, blanko karaktera ima: %d", c, b);
+
+	if (Niz1 == Niz2) { 
+		printf("\nNizovi su isti.");
+	}
+	else {
+		printf("\nNizovi nisu isti.");
+	}
+}
+
+void z10() {
+	/* u programu se unose podaci za N lopti i stampaju se vrednosti njihovih 
+	povrsina i zapremina. povrsina i zapremina se racunaju pomocu zasebnih funkcija. */
+
+	int n, r;
+	float p, z;
+	printf("Unesi N: "); scanf("%d", &n); // broj lopti 
+
+	for (int i = 0; i < n; i++) {
+		printf("Unesi r: "); scanf("%d", &r); // unos poluprecnika lopte
+		printf("Povrsina: %f, Zapremina: %f", povrsina_lopte(r), zapremina_lopte(r));
+	}
+}
+
+
+int main() {
+
+	// printf("S = %d", z1());
+
+	// printf("Unesi cifru (0-9): ");  z3();
+	
+	// printf("Zbir unetih brojeva: %d", z5());
+
+	// printf("\n"); z6();
+
+	//z8("ulaz.txt", "izlaz.txt");
+
+	// z9();
+
+	z10();
+
+}
